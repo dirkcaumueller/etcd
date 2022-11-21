0d8a84703c8ab5d7c60ba295e21225c84dbfd584
Dirk Aumueller <d.aumueller@proventa.de>
2022-11-21 12:48:04 +0100

    Initial POC setup

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # etcd
-Run etcd in a Rockylinux Container.
+
+Run [etcd](https://etcd.io/) in a Red Hat ubi-minimal container.
+
+## Prerequisites
+
+Install required packages.
+
+```bash
+sudo apt update
+sudo apt install podman buildah python3-pip
+pip3 install podman-compose
+```
+
+## Create Container
+
+Switch to user `root`.
+
+```bash
+sudo su -
+```
+
+Check if etcd container/image already exists and remove them
+
+```bash
+buildah rm etcd
+buildah rmi etcd
+```
+
+Run bash script to create container image.
+
+```bash
+./create_image.sh
+```
+
+Check if container image exists.
+
+```bash
+buildah images
+```
+
+## Run 3-node etcd Test Cluster
+
+Create a 3-node etcd Cluster to test the container.
+
+```bash
+podman-compose up -d
+```
+
+Check the cluster member list.
+
+```bash
+ETCDCTL_API=3 etcdctl --endpoints localhost:23790,localhost:23791,localhost:23792 member list
+```
+
+And validate the cluster health status.
+
+```bash
+ETCDCTL_API=3 etcdctl --endpoints localhost:23790,localhost:23791,localhost:23792 endpoint health
+```
---
+++ create_image.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Set etcd version and download URL
+ETCD_VERSION=v3.5.5
+DOWNLOAD_URL=https://github.com/etcd-io/etcd/releases/download
+
+# Download and extract etcd
+curl -L ${DOWNLOAD_URL}/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz -o etcd-${ETCD_VERSION}-linux-amd64.tar.gz
+
+# Extract etcd binaries
+tar -zxvf etcd-${ETCD_VERSION}-linux-amd64.tar.gz \
+    etcd-${ETCD_VERSION}-linux-amd64/etcd \
+    etcd-${ETCD_VERSION}-linux-amd64/etcdctl \
+    etcd-${ETCD_VERSION}-linux-amd64/etcdutl \
+    --strip-components=1
+
+# Create a minimal container based on ubi-minimal image
+newcontainer=$(buildah from --name=etcd registry.access.redhat.com/ubi8/ubi-minimal)
+
+# Mount container and install basics
+scratchmnt=$(buildah mount $newcontainer)
+
+# Configure the container
+buildah config --created-by "Dirk Aumueller"  $newcontainer
+buildah config --author "Dirk Aumueller" --label name=dca $newcontainer
+
+# Create etcd group and user
+buildah run $newcontainer microdnf install shadow-utils libsemanage
+buildah run $newcontainer groupadd etcd
+buildah run $newcontainer useradd -g etcd -m etcd
+buildah run $newcontainer microdnf remove shadow-utils libsemanage
+buildah run $newcontainer microdnf clean all
+buildah config --user etcd $newcontainer
+buildah config --workingdir /home/etcd $newcontainer
+
+# Create a data directory
+buildah run --user root $newcontainer mkdir -p /etcddata
+buildah run --user root $newcontainer chown etcd:etcd /etcddata
+buildah config --env ETCD_DATA_DIR="/etcddata" $newcontainer
+
+# Copy etcd binaries into image
+buildah copy $newcontainer ./etcd /usr/local/bin/etcd
+buildah copy $newcontainer ./etcdctl /usr/local/bin/etcdctl
+buildah copy $newcontainer ./etcdctl /usr/local/bin/etcdutl
+
+# Set entrypoint
+buildah config --entrypoint /usr/local/bin/etcd $newcontainer
+
+# Unmount container and create image
+buildah unmount $newcontainer
+buildah commit $newcontainer etcd:${ETCD_VERSION}
+
+# Remove downloaded etcd binaries
+rm -f etcd-${ETCD_VERSION}-linux-amd64.tar.gz etcd etcdctl etcdutl
